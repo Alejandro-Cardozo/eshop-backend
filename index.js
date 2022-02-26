3cae01b3e40b9e1ebdb8c36f1343dd11f4d832b1
Alejandro Cardozo <uri.cardozo@gmail.com>
2022-02-26 20:33:25 -0300

    DB connection and testing

--- index.js
+++ index.js
@@ -1,15 +1,71 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+// setup
 const app = express();
-
 require('dotenv/config');
-
 const api = process.env.API_URL;
+const user = process.env.DB_USER;
+const cluster = process.env.DB_NAME;
+const collection = process.env.DB_COLLECTION;
+const password = process.env.DB_PASSWORD;
 
-app.get(api + '/products', (req, res) => {
-  res.send('hello API!');
-})
+//Middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+
+const productSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  countInStock: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+app.get(`${api}/products`, async (req, res) => {
+  const productList = await Product.find();
+
+  if (!productList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(productList);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock,
+  });
+
+  product
+    .save()
+    .then((createdProduct) => {
+      res.status(201).json(createdProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        success: false,
+      });
+    });
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://${user}:${password}@${cluster}.tgo7u.mongodb.net/${collection}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log('Database Connection is ready...');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(8080, () => {
-  console.log(api)
-  console.log('server is running http://localhost:8080')
-})
+  console.log('server is running http://localhost:8080');
+});
