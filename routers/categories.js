815450b79337b42fd4ce6340136f5f8116f4a29b
Alejandro Cardozo <uri.cardozo@gmail.com>
2022-02-27 19:28:27 -0300

    Get single category and update category

--- routers/categories.js
+++ routers/categories.js
@@ -8,7 +8,34 @@ router.get(`/`, async (req, res) => {
   if (!categoryList) {
     res.status(500).json({ success: false });
   }
-  res.send(categoryList);
+  res.status(200).send(categoryList);
+});
+
+router.get('/:id', async (req, res) => {
+  const category = await Category.findById(req.params.id);
+
+  if (!category) {
+    res
+      .status(500)
+      .json({ message: 'The category with the given ID was not found' });
+  }
+  res.status(200).send(category);
+});
+
+router.put('/:id', async (req, res) => {
+  const category = await Category.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      icon: req.body.icon,
+      color: req.body.color,
+    },
+    { new: true }
+  );
+
+  if (!category) return res.status(404).send('the category cannot be updated!');
+
+  res.send(category);
 });
 
 router.post('/', async (req, res) => {
