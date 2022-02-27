93a2927ee4297114c391658517584713d3203cae
Alejandro Cardozo <uri.cardozo@gmail.com>
2022-02-27 13:04:20 -0300

    Product model/schema

--- models/product.js
+++ models/product.js
@@ -1,12 +1,42 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required: true
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  richDescription: {
+    type: String,
+    default: '',
+  },
+  image: {
+    type: String,
+    default: '',
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255,
+  },
+});
 
 exports.Product = mongoose.model('Product', productSchema);
