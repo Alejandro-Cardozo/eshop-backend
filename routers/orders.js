4cb34ef2579c819714021c3da9f451a8722da521
Alejandro Cardozo <uri.cardozo@gmail.com>
2022-03-04 16:18:31 -0300

    GET (populated) Order detail

--- routers/orders.js
+++ routers/orders.js
@@ -3,13 +3,31 @@ const { OrderItem } = require('../models/order-item');
 const express = require('express');
 const router = express.Router();
 
+// GET all orders
 router.get(`/`, async (req, res) => {
-  const orderList = await Order.find();
+  const orderList = await Order.find()
+    .populate('user', 'name')
+    .sort({ dateOrdered: -1 });
 
   if (!orderList) {
     res.status(500).json({ success: false });
   }
   res.send(orderList);
+});
+
+// GET single order
+router.get(`/:id`, async (req, res) => {
+  const order = await Order.findById(req.params.id)
+    .populate('user', 'name')
+    .populate({
+      path: 'orderItems',
+      populate: { path: 'product', populate: 'category' },
+    });
+
+  if (!order) {
+    res.status(500).json({ success: false });
+  }
+  res.send(order);
 });
 
 // POST new category
