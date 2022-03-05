f5676547c8c9c1591f9be403d55f0dd0a4df4c92
Alejandro Cardozo <uri.cardozo@gmail.com>
2022-03-05 17:20:07 -0300

    Image upload implementation

--- routers/products.js
+++ routers/products.js
@@ -3,13 +3,39 @@ const { Category } = require('../models/category');
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+
+const FILE_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpeg',
+};
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const isValid = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error('invalid image type');
+
+    if (isValid) {
+      uploadError = null;
+    }
+    cb(uploadError, 'public/uploads');
+  },
+  filename: function (req, file, cb) {
+    const fileName = file.originalname.split(' ').join('-');
+    const extension = FILE_TYPE_MAP[file.mimetype];
+    cb(null, `${fileName}-${Date.now()}.${extension}`);
+  },
+});
+
+const uploadOptions = multer({ storage: storage });
 
 // GET all products
 router.get(`/`, async (req, res) => {
   let filter = {};
 
-  if(req.query.categories) {
-    filter = {category: req.query.categories.split(',')}
+  if (req.query.categories) {
+    filter = { category: req.query.categories.split(',') };
   }
 
   /* get just specific fields  
@@ -35,15 +61,21 @@ router.get(`/:id`, async (req, res) => {
 });
 
 //POST new product
-router.post(`/`, async (req, res) => {
+router.post(`/`, uploadOptions.single('image'), async (req, res) => {
   const category = await Category.findById(req.body.category);
   if (!category) return res.status(400).send('Invalid category');
+
+  const file = req.file;
+  if (!file) return res.status(400).send('No image found in the request');
+
+  const fileName = req.file.filename;
+  const basePath = `${req.protocol}://${req.get('host')}/public/upload/`;
 
   let product = new Product({
     name: req.body.name,
     description: req.body.description,
     richDescription: req.body.richDescription,
-    image: req.body.image,
+    image: `${basePath}${fileName}`,
     images: req.body.images,
     brand: req.body.brand,
     price: req.body.price,
